031e9d78788e82c4a11ef1e6d84cac40aa741814
iamsupercharged <rayan@supercharged.gg>
2021-02-12 09:57:29 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { AppRoutes } from './app.routing';
+
+import { SceneComponent } from './scene/scene.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SceneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
